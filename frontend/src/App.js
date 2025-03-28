4d01236a7efd719039c952e9a2c96a54ca0d8277
IRO05 <isaacedwardrobinson@gmail.com>
2025-03-28 17:32:54 -0400

    changed navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
             <Route path="/category1" element={<Category1 />} />
             <Route path="/category2" element={<Category2 />} />
             <Route path="/category3" element={<Category3 />} />
-            <Route path="/category4" element={<Calendar />} />
+            <Route path="/calendar" element={<Calendar />} />
             <Route path="/ai" element={<AI />} />
           </Routes>
         </div>
