345a02cbc93e3d7f39ae276b299625d4450e3e72
Valery Louis <louisvalery2022@gmail.com>
2025-03-29 07:05:07 -0400

    new nice frontend :)

--- frontend/src/pages/Category2.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CategoryPage from '../components/CategoryPage';
-
-const Category2 = () => {
-  return <CategoryPage categoryId="category2" title="Category 2" />;
-};
-
-export default Category2;
